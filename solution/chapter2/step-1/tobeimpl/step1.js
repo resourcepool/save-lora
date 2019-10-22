8a8e788a81c858b750307be8eb4b13575fceb5ab
loic ortola <contact@loicortola.com>
2019-10-22 14:24:04 +0200

    Added solution for chapter2 - step-1

---
+++ solution/chapter2/step-1/tobeimpl/step1.js
@@ -0,0 +1,48 @@
+const conf = require("../conf");
+let serialComService = require("../serial-com");
+
+/**
+ *
+ * Here, it's quite simple.
+ *
+ * You need to implement those 4 methods.
+ * You only have to replace CHANGEME by the correct (and full) AT command
+ *
+ * a valid command starts with "at+"
+ *
+ * There is no test to help you validating those steps. But, really, the only thing you have to do is read examples, find the good one, and use it.
+ *
+ * Just, don't forget to use configurations values appEUI and appKey (a local variable is provided in each method needing it)
+ *
+ * And since there is no tests to help you validate those steps, don't hesitate to ask for a safety check if you don't succeed to validate the challenge (or if you are stuck)
+ *
+ */
+
+const setModeLoraWan = () => {
+    serialComService.sendCommand("at+mode=0");
+};
+
+const setAppEui = () => {
+    const appEUI = conf.appEUI;
+    serialComService.sendCommand("at+set_config=app_eui:" + appEUI);
+};
+
+const setAppKey = () => {
+    const appKey = conf.appKey;
+    serialComService.sendCommand("at+set_config=app_key:" + appKey);
+};
+
+
+const sendJoinRequest = () => {
+    console.debug("Setting Join Request");
+    serialComService.sendCommand("at+join=otaa");
+};
+
+
+
+module.exports = {
+    setModeLoraWan,
+    setAppEui,
+    setAppKey,
+    sendJoinRequest
+};
